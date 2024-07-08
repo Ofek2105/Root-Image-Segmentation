ee66032e7187bf6459914bc489be3a6bb19c75fc
Ofek Nourian <ooffeekkii@gmail.com>
2024-07-08 14:17:44 +0300

    fixed class ordering

--- train_seg.py
+++ train_seg.py
@@ -1,11 +1,9 @@
 from ultralytics import YOLO
 
-
 def main():
-    # Load a pretrained model (recommended for training)
-    model = YOLO('yolov8l-seg.pt')
-    results = model.train(data='dataset.yaml', epochs=200, imgsz=300)
-
+    # Load a pretrained model (recommended for training) 'yolov8m-seg.pt'
+    model = YOLO('yolov8m-seg.pt')
+    results = model.train(data='dataset.yaml', epochs=150, imgsz=320, device='cuda')
 
 if __name__ == '__main__':
     main()
