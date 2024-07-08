6b3a864cedee101728bc6911f074f1c7a976c7ee
Ofek Nourian <ooffeekkii@gmail.com>
2024-07-08 14:14:13 +0300

    different comments

--- json2coco.py
+++ json2coco.py
@@ -2,8 +2,8 @@ import splitfolders
 from ultralytics.data.converter import convert_coco
 
 # convert Json to labels
-convert_coco(labels_dir='JSON/DIR', use_segments=True)
+# convert_coco(labels_dir='dataset/images', use_segments=True)
 
 # split into train validation testing
-input_folder = "coco_converted"
-splitfolders.ratio(input_folder, output="dataset", seed=1337, ratio=(.7, .2, .1), group_prefix=None, move=False)
+input_folder = "dataset"
+splitfolders.ratio(input_folder, output="dataset_split", seed=1337, ratio=(.7, .2, .1), group_prefix=None, move=False)
