6b58a676868a743b3957a6c21f04f3daf0ecda78
Ofek Nourian <ooffeekkii@gmail.com>
2024-09-29 10:31:47 +0300

    changed parameters

--- train_seg.py
+++ train_seg.py
@@ -2,8 +2,24 @@ from ultralytics import YOLO
 
 def main():
     # Load a pretrained model (recommended for training) 'yolov8m-seg.pt'
-    model = YOLO('yolov8m-seg.pt')
-    results = model.train(data='dataset.yaml', epochs=150, imgsz=320, device='cuda')
+    model = YOLO('YOLOv8m-seg.pt')
+    results = model.train(
+        data='dataset.yaml',
+        epochs=300,
+        imgsz=960,
+        device='cuda',
+        batch= 4
+    )
 
 if __name__ == '__main__':
     main()
+    """
+    changes this run: 
+    -   new version of ultralytics
+    -   larger imagez from 320 to 1024
+    -   model size from x to m
+    -   added weights: [2.0, 1.0]
+    
+    suggestion for next:
+    -   change weights to: [1.5, 1.0]
+    """
